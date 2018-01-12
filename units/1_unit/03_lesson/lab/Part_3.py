93d3870765ff8446c34cc4003bd0f8d4bcd17631
Brandon Perkins <bperkins@redhat.com>
2018-01-12 17:26:56 -0500

    Part 3

---
+++ units/1_unit/03_lesson/lab/Part_3.py
@@ -0,0 +1,11 @@
+#! /usr/bin/python3
+
+print(" Zero is",  4 / 4 * 4 - 4  )
+
+print("  One is",  4 / 4 + 4 - 4  )
+
+print("  Two is",  4 -(4 + 4)/ 4  )
+
+print("Three is", (4 * 4 - 4)/ 4  )
+
+print(" Four is",  4 + 4 *(4 - 4) )
