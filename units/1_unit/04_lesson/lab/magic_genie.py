68cc2a0e8a0c9234db761133f67328e4ad4f8614
Brandon Perkins <bperkins@redhat.com>
2018-01-16 16:55:27 -0500

    Magic Genie

---
+++ units/1_unit/04_lesson/lab/magic_genie.py
@@ -0,0 +1,11 @@
+#! /usr/bin/python3
+
+print("I am a genie. You have three wishes")
+
+wishtext = "What would you like to wish for? "
+
+wish1 = input(wishtext)
+wish2 = input(wishtext)
+wish3 = input(wishtext)
+
+print("Your wishes are " + wish1 + ", " + wish2 + ", and " + wish3)
